c76fdb53a4c6dd7050a745155b2b58980c7bfa2d
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2023-03-22 11:20:55 -0400

    add random functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,38 @@ const App = () => {
   const [index, setIndex] = useState(0)
   const {name, job, image, text} = people[index]
 
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number
+  }
+
+  const nextPerson = () => {
+    setIndex((currentIndex) =>{
+      const newIndex = currentIndex +1
+      return checkNumber(newIndex)
+    })
+  }
+  
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      return checkNumber(newIndex);
+    });
+  }
+
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length)
+    if(randomNumber === index){
+      randomNumber = index + 1
+    }
+    setIndex(checkNumber(randomNumber))
+  }
+
   return (
     <main>
       <article className="review">
@@ -18,6 +50,17 @@ const App = () => {
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>
         <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="prev-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className='btn btn-hipster' onClick={randomPerson}>
+          Surprise Me
+        </button>
       </article>
     </main>
   );
